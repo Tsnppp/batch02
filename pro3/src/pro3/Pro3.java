86d0697bd6f6914218d688cca10d8765cbe9a1e0
SUBHASP <subhasp94@gmail.com>
2025-01-09 12:41:37 +0530

    commit

--- pro3/src/pro3/Pro3.java
+++ pro3/src/pro3/Pro3.java
@@ -5,6 +5,8 @@ public class Pro3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helooo");
+		System.out.println("hiiiii dear");
+	
 	}
 
 }
